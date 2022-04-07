6f6fbf15dc4c876ee014c737dd0803260fc5077d
PY <181250111@smail.nju.edu.cn>
2022-04-07 21:08:11 +0800

    feat: 配置CORS

---
+++ src/main/java/com/panyue/judgmentdoc/config/CORSConfig.java
@@ -0,0 +1,34 @@
+package com.panyue.judgmentdoc.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author: panyue
+ * @create: 2022-04-07
+ */
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");  //本地前端
+        config.addAllowedOrigin("http://localhost:8099");  //本地Tomcat
+//        config.addAllowedOrigin("");  //部署前端
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
+}
